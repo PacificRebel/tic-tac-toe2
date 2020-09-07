1649b4a692e782eea6f239cd2b0aea1d4da6146e
Heli Sivunen <heli@mooncat.tv>
2020-09-07 15:58:49 +0100

    Basic pages built and index running in browser.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// this file is a React component
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+        </div>
+    );
+  }
+}
+
+export default App;
